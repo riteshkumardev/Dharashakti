1f8359dd2fdb459c280660451ba127e68cec3b4d
Ritesh <ritesh@example.com>
2026-01-21 21:07:43 +0530

    Dashbord update

--- backend/controllers/employees.controller.js
+++ backend/controllers/employees.controller.js
@@ -45,8 +45,8 @@ export const updateEmployeeStatus = async (req, res) => {
 };
 export const deleteEmployee = async (req, res) => {
   try {
-    const { id } = req.params;
-    await Employee.findByIdAndDelete(id);
+    const { id } = req.params;
+   await Employee.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Employee Deleted" });
   } catch (error) {
     res.status(500).json({ success: false, message: "Delete failed" });
